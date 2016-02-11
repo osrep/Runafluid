a571932d73d8ace37d9b498a1c682a19f275cef7
plasmatyi <aradimatyi@gmail.com>
2016-02-11 16:15:27 +0000

    Avalanche rate module editing

--- avalanche_rate.h
+++ avalanche_rate.h
@@ -4,7 +4,7 @@
 #include "constants.h"
 
 int is_field_critical(profile pro);
-
-double avalanche_generation_rate(double electron_density, double electron_temperature,	double effective_charge, double electric_field, double dt)
+double calculate_critical_field(double electron_density, double electron_temperature);
+double avalanche_generation_rate(double electron_density, double electron_temperature,	double effective_charge, double electric_field, double dt);
 
 #endif /* AVALANCHE_RATE_H_ */
