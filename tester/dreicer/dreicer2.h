c435846c939d7c724a31bb2ea151f91e5bc84dcf
plasmatyi <aradimatyi@gmail.com>
2016-02-15 15:22:47 +0000

    #924 Dreicer module tester editing

---
+++ tester/dreicer/dreicer2.h
@@ -0,0 +1,11 @@
+#ifndef DREICER_H_
+#define DREICER_H_
+
+#include "constants.h"
+
+int is_field_critical(profile pro);
+
+double calculate_critical_field(double electron_density, double electron_temperature);
+double dreicer_generation_rate(double electron_density, double electron_temperature, double effective_charge, double electric_field);
+
+#endif /* DREICER_H_ */
