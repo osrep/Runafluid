c1ef2671f6870ce1c79cac0f3c17ffd7d6e952ae
plasmatyi <aradimatyi@gmail.com>
2016-02-15 13:04:52 +0000

    #924 Dreicer module tester editing

--- tester/dreicer/main.cpp
+++ tester/dreicer/main.cpp
@@ -14,9 +14,9 @@ using namespace std;
 int main(void){
 
 	double electron_density = 1e20;//e20;//m^-3
-	double electron_temperature = 120.0;//eV
+	double electron_temperature = 1606;//eV
 	double effective_charge  = 	2.0;
-	double electric_field = 100;//3.930;// V/m
+	double electric_field = 3.991;// V/m
 	
 	double rate=-999999999;
 	rate = dreicer_generation_rate(electron_density, electron_temperature,
