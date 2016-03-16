8b35841f0b656ac7b02ac4e88e156e76ad40898f
plasmatyi <aradimatyi@gmail.com>
2016-03-16 13:54:07 +0000

    [#924] avalanche modul -- bug fixed

--- avalanche_rate.h
+++ avalanche_rate.h
@@ -6,6 +6,6 @@
 int is_field_critical(profile pro);
 int init_avalanche(profile pro, double Ea);
 double calculate_critical_field(double electron_density, double electron_temperature);
-double avalanche_generation_rate(double electron_density, double electron_temperature,	double effective_charge, double electric_field, double Ea, double dt);
+double avalanche_generation_rate(double electron_density, double electron_temperature,	double effective_charge, double electric_field, double Ea);
 
 #endif /* AVALANCHE_RATE_H_ */
