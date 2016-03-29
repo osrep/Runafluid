0af2ae8dd1e94d1180183e34a0ca95c9e9c8c5a9
plasmatyi <aradimatyi@gmail.com>
2016-03-29 11:38:01 +0000

    [#924] Runafluid test actor (for manual editing) / added

---
+++ test/distredit.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <UALClasses.h>
+
+
+/*! 
+
+runaway distribution reader
+
+*/
+
+void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
+		ItmNs::Itm::equilibrium &equilibrium, ItmNs::Itm::distribution &distribution) {
+		
+		
+		try {
+			
+		double rundensity = 0.0;
+		double rundensity2 = 1234.56;
+		
+		//! reading profile from CPO inputs
+		profile pro = cpo_to_profile(coreprof, coreimpur, equilibrium, distribution);
+		
+		//! stepping iterator in profile		
+		for (std::vector<cell>::iterator it = pro.begin(); it != pro.end(); ++it) {
+			//rundensity = 1234.5678;//
+			rundensity = it->runaway_density;
+			distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho) = rundensity2;
+		
+		
+		}		
+		
+				
+	
+
+	} catch (const std::exception& ex) {
+		std::cerr << "ERROR An error occurred during firing actor Runafluid." << std::endl;
+		std::cerr << "ERROR : " << ex.what() << std::endl;
+		
+		//! internal error in distribution
+		
+	}
+
+}
