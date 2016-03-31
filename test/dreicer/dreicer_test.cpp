a886de27155ab8fed315ef2513f1cf6ae537f8e6
plasmatyi <aradimatyi@gmail.com>
2016-03-31 14:03:52 +0000

    [#924] Dreicer generation tester

--- test/dreicer/dreicer_test.cpp
+++ test/dreicer/dreicer_test.cpp
@@ -26,7 +26,7 @@ int main(){
 	try {
 	
 		//! Initialise data file
-		std::ofstream datafile, datafilog, dataf0, dataf1, dataf2;
+		std::ofstream datafile, datafilog, dataf0, dataf1, dataf2, dataf0l;
   		datafile.open ("dreicer.dat");
   		datafilog.open ("dreicer_log.dat");
   		
@@ -38,35 +38,51 @@ int main(){
   		
   		
   		dataf0.open ("dreicer0.dat");	
+  		dataf0l.open ("dreicer0l.dat");
   		
   		dataf1.open ("dreicer1.dat");
   		dataf2.open ("dreicer2.dat");
   		
 		for (int i=0;i<50;i++){
-			electron_temperature = pow(10,i/10);			
+			electron_temperature = pow(10,i/10.0);			
 			dataf2 << electron_temperature << "\t";
 			
   			for (int j=0;j<50;j++){
   			
-  				electric_field= pow(10,j/10-2);
+  				electric_field= pow(10,j/10.0-2.0);
 			//! Calculate Dreicer generation rate
 				rate_dreicer = dreicer_generation_rate(electron_density, electron_temperature,
 				effective_charge, electric_field);
-		
+				
+				if (isnan(rate_dreicer)){
+					rate_dreicer =1e99;
+				}
+				
 				if (j==0){
 					dataf0 << rate_dreicer;
+					dataf0l << log10(rate_dreicer);
 				}else{	
 					dataf0 << "\t" << rate_dreicer ;
+					dataf0l << "\t" << log10(rate_dreicer) ;
 				}
+				
+				
+				
 				datafile << electric_field << "\t" << electron_temperature << "\t"  << rate_dreicer  << "\n";
 				datafilog << log10(electric_field) << "\t" << log10(electron_temperature) << "\t"  << log10(rate_dreicer)  << "\n";
 				
 			}
 			dataf0 << "\n";
+			dataf0l << "\n";
 		}
 		//datafile << "test\n";
 		//! close data writing
 		datafile.close();
+		datafilog.close();
+		dataf0.close();
+		dataf0l.close();
+		dataf1.close();
+		dataf2.close();
 			
 				
 
