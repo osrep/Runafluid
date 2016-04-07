5f1d26e9c05da4cb9c0213c04635530d9d421d4d
plasmatyi <aradimatyi@gmail.com>
2016-04-07 12:39:57 +0000

    [#924] electric field editor test modul (fc2k)

--- test/efieldedit.cpp
+++ test/efieldedit.cpp
@@ -8,25 +8,66 @@
 #include "../init.h"
 #include "../cpo_utils.h"
 
-
 /*! 
 
 runaway distribution editor
 
 */
 
-void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
-		ItmNs::Itm::equilibrium &equilibrium, ItmNs::Itm::distribution &distribution, double &temp) {
+profile read_coreprof(const ItmNs::Itm::coreprof &coreprof) {
+
+	profile pro;
+
+	//! read electron density profile length of dataset: cells	
+	int cells = coreprof.ne.value.rows();
+	
+	//! read electron temperature profile length of dataset, comparing with cells
+	if (coreprof.te.value.rows() != cells)
+		throw std::invalid_argument("Number of values is different in coreprof ne and te.");
+
+	//! read eparallel profile length of dataset, comparing with cells
+	if (coreprof.profiles1d.eparallel.value.rows() != cells)
+		throw std::invalid_argument(
+				"Number of values is different in coreprof.ne and coreprof.profiles1d.eparallel.");
+
+    //! read data in every $\rho$ 
+
+	for (int rho = 0; rho < cells; rho++) {
+		cell celll;
+				
+		//! electron density
+		celll.electron_density = coreprof.ne.value(rho);
+		
+		//! electron temperature
+		celll.electron_temperature = coreprof.te.value(rho);
+		
+		/*! local electric field
+			\f[ E = \frac{E_\parallel(\rho) B_0}{B_\mathrm{av}(\rho)} \f]
+			where B_\mathrm{av} is known on discreate \f$R \f$ major radius and interpolated at $\rho$ normalised minor radius
+		*/
+		celll.electric_field = coreprof.profiles1d.eparallel.value(rho) /** coreprof.toroid_field.b0
+				/ interpolate(equilibrium.profiles_1d.rho_tor, equilibrium.profiles_1d.b_av,
+						coreprof.rho_tor(rho))*/;
+
+
+		
+
+		pro.push_back(celll);
+	}
+
+	return pro;
+}
+
+
+void fire(ItmNs::Itm::coreprof &coreprof) {
 		
 		
 		try {
 			
 		int rho = 0;
-			
-		temp = 0;
-		
+					
 		//! reading profile from CPO inputs
-		profile pro = cpo_to_profile(coreprof, coreimpur, equilibrium, distribution);
+		profile pro = read_coreprof(coreprof);
 		
 		//! stepping iterator in profile		
 		for (std::vector<cell>::iterator it = pro.begin(); it != pro.end(); ++it) {
