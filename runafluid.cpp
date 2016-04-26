6f675bc6ee0f9c7e6c5c7a353a507b10363c88dc
plasmatyi <aradimatyi@gmail.com>
2016-04-26 14:33:49 +0000

    [#924] distri_vec array resize -- test 003

--- runafluid.cpp
+++ runafluid.cpp
@@ -105,7 +105,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 	}
 	
 	try {
-		distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens.array.resize(3);
+		distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens.resize(3);
 	} catch (const std::exception& ex) {
 		std::cerr << "ERROR An error occurred during distribution array resize" << std::endl;
 		std::cerr << "ERROR : " << ex.what() << std::endl;
