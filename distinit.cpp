f287060aa4a21ab306f9f7be58d769b028ea0911
plasmatyi <aradimatyi@gmail.com>
2016-04-28 10:30:08 +0000

    [#924] distinit actor modified

--- distinit.cpp
+++ distinit.cpp
@@ -14,6 +14,17 @@
 void fire(ItmNs::Itm::distribution &dist_in, ItmNs::Itm::distribution &dist_out, int &dist_initialised) {
 
 
+//std::cerr << "Number of elements:"<< std::endl;
+//distri_vec[*]/source_id/type/flag == 7
+
+	//! length of distri_vec
+	int N_dist = dist_in.distri_vec.rows();
+	
+	std::cerr << "Length of distri_vec: \t"<< N_dist << std::endl << std::endl;
+	
+	for (int i=0;i<N_dist;i++){
+		std::cerr << "\t"<< i << ": \t" << dist_in.distri_vec.rows(i).source_id.type.flag.value() << std::endl;
+	}
 	
 
 }
