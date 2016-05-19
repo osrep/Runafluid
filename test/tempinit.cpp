f6e6183c7dfc25f978b90651382362b7f65229e8
plasmatyi <aradimatyi@gmail.com>
2016-05-19 14:59:32 +0000

    [#924] Runafluid | tempInit module added

---
+++ test/tempinit.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <UALClasses.h>
+
+void fire(ItmNs::Itm::temporary &tempData) {
+	tempData.timed.float2d.resize(1);
+}
