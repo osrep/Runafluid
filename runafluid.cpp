5fcffbfdc5a0ee8addf8ebadc52bcbedc9e43532
plasmatyi <aradimatyi@gmail.com>
2016-05-19 16:10:29 +0000

    [#924] Runafluid | big fix | minus distribution handled

--- runafluid.cpp
+++ runafluid.cpp
@@ -123,7 +123,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 			//std::cerr << "OUT : " << rundensity << std::endl;
 			
 			if (rho<distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens.rows()){
-				if(rundensity < it->electron_density){
+				if(rundensity < it->electron_density || rundensity < 0){
 			   		distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho) = rundensity;
 		   		}else{
 			   		distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho) = it->electron_density;
