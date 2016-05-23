c1890a22c95f94f4db443703a9684c92744728e2
plasmatyi <aradimatyi@gmail.com>
2016-05-23 12:46:11 +0000

    [#924] Dreicer module bug fix

--- dreicer.cpp
+++ dreicer.cpp
@@ -184,7 +184,7 @@ double dreicer_generation_rate(double electron_density, double electron_temperat
 	\f]
 	*/		
 		
-	double dgr = Cr*electron_density/tao * pow(Edn,-h) * exp(-lambda/4/Edn - sqrt(2/Edn)*gamma);
+	double dgr = Cr*electron_density/tao * pow(Edn,h) * exp(-lambda/4/Edn - sqrt(2/Edn)*gamma); // -h -> h
 	
 	//Dreicer generation rate
 	return dgr;
