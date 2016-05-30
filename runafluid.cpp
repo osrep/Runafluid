2b76dffb81ff04fd72449f42ff1b319d2b3bdbde
plasmatyi <aradimatyi@gmail.com>
2016-05-30 15:24:32 +0000

    [#924] debugging | Runafluid: distributionInit

--- runafluid.cpp
+++ runafluid.cpp
@@ -128,7 +128,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		
 		
 		//! set distribution CPO with default data
-		distributionInit(distribution_prev, distribution, coreprof);
+//		distributionInit(distribution_prev, distribution, coreprof);
 		
 		//! reading profile from CPO inputs (cpo_utils.h)
 		profile pro = cpo_to_profile(coreprof, coreimpur, equilibrium, distribution);
