67d25c709b84e23bccf35cac00904ed8e8ce6aa2
plasmatyi <aradimatyi@gmail.com>
2016-06-02 18:38:56 +0000

    [#924] distribution initialiser | area+volume

--- distinit.cpp
+++ distinit.cpp
@@ -121,11 +121,11 @@ void fire(ItmNs::Itm::distribution &distribution_in, ItmNs::Itm::distribution &d
 			}
 			
 			if (i < N_area){
-//				distribution_out.distri_vec(0).profiles_1d.geometry.area(i) = coreimpur.area(i);
+				distribution_out.distri_vec(0).profiles_1d.geometry.area(i) = coreimpur.area(i);
 			}
 			
 			if (i < N_volume){
-//				distribution_out.distri_vec(0).profiles_1d.geometry.volume(i) = coreimpur.volume(i);
+				distribution_out.distri_vec(0).profiles_1d.geometry.volume(i) = coreimpur.volume(i);
 			}
 		}
 		
