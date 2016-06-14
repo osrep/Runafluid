c291ec9986757dd9a3aefa170b1ff001a596f2da
plasmatyi <aradimatyi@gmail.com>
2016-06-14 02:33:53 +0000

    [#924] New distribution slicer

--- test/nothing.cpp
+++ test/nothing.cpp
@@ -1,3 +1,9 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <UALClasses.h>
+
 void fire(ItmNs::Itm::equilibrium &equilibrium, ItmNs::Itm::distribution &distribution){
 
 }
