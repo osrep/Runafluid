3962bc7611f99318753ca80714b2b6ad8f40f441
plasmatyi <aradimatyi@gmail.com>
2016-06-16 16:09:55 +0000

    [#924] Previous distribution as temporary cpo set up | bug fix

--- runafluid.cpp
+++ runafluid.cpp
@@ -89,7 +89,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		double rundensity = 0.0;
 		
 		//! Number of rate calculations (Dreicer, Avalanche etc.)
-		int N_rates = 2;//5;
+		int N_rates = 5;
 		double rate_values[N_rates];
 		
 		
@@ -112,7 +112,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		runaway_rates.timed.float1d(0).identifier.description = "Dreicer generation rate";
 		runaway_rates.timed.float1d(0).value.resize(N_rho);		
 		
-		/*runaway_rates.timed.float1d(2).identifier.id = "dreicerT";
+		runaway_rates.timed.float1d(2).identifier.id = "dreicerT";
 		runaway_rates.timed.float1d(2).identifier.flag = 2;
 		runaway_rates.timed.float1d(2).identifier.description = "Dreicer generation rate TEMP";
 		runaway_rates.timed.float1d(2).value.resize(N_rho);				
@@ -125,7 +125,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		runaway_rates.timed.float1d(4).identifier.id = "dreicerP";
 		runaway_rates.timed.float1d(4).identifier.flag = 4;
 		runaway_rates.timed.float1d(4).identifier.description = "Dreicer generation rate PREV";
-		runaway_rates.timed.float1d(4).value.resize(N_rho);*/
+		runaway_rates.timed.float1d(4).value.resize(N_rho);
 		
 		//! Avalanche generation rate initialisation
 		runaway_rates.timed.float1d(1).identifier.id = "avalanche";
@@ -153,11 +153,11 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 				//! calculating runaway density
 				rundensity = runafluid_control(it->electron_density, it->runaway_density, it->electron_temperature, it->effective_charge, it->electric_field, timestep, runafluid_switch, rate_values);
 			   	
-			   /*	if(rho<Ntemp){
+			   	if(rho<Ntemp){
 				   	rate_values[2]=distribution_temp.non_timed.float1d(0).value(rho);
-				   	rate_values[3]=distribution_tempA[0].timed.float1d(0).value(rho);
+				   	rate_values[3]=0;//distribution_tempA[0].timed.float1d(0).value(rho);
 				   	rate_values[4]=distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho);
-			   	}*/
+			   	}
 			   	
 			   	//! CPO output
 			   	distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho) = rundensity;
