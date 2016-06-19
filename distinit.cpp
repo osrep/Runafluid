bd888534ad5f6a84b665c71f15ffe6eb0da629cd
plasmatyi <aradimatyi@gmail.com>
2016-06-19 13:26:42 +0000

    [#924] distInit | new port structure | port simplifying and code cleaning

--- distinit.cpp
+++ distinit.cpp
@@ -13,7 +13,7 @@
 
 
 
-void fire(ItmNs::Itm::distribution &distribution_in, ItmNs::Itm::distribution &distribution_out, ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur, int &dist_initialised, ItmNs::Itm::temporary &distribution_temp/*, ItmNs::Itm::temporaryArray &distribution_tempA*/) {
+void fire(ItmNs::Itm::distribution &distribution, ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur, ItmNs::Itm::temporary &distribution_temp/*, ItmNs::Itm::temporaryArray &distribution_tempA*/) {
 
 	
 	try {
@@ -38,7 +38,7 @@ void fire(ItmNs::Itm::distribution &distribution_in, ItmNs::Itm::distribution &d
 	//std::cerr << "Length of distri_vec: \t"<< N_volume << std::endl << std::endl;
 	
 		int flag = 0;
-		std::cerr << "dens3prev: " << distribution_in.distri_vec(0).profiles_1d.state.dens(3) << std::endl;
+		/*std::cerr << "dens3prev: " << distribution_in.distri_vec(0).profiles_1d.state.dens(3) << std::endl;
 				
 		try {			
 			flag = distribution_in.distri_vec(0).source_id(0).type.flag;			
@@ -51,98 +51,98 @@ void fire(ItmNs::Itm::distribution &distribution_in, ItmNs::Itm::distribution &d
 		if (dist_initialised & flag==7){
 			std::cerr << " -----------  PREVIOUS DISTRIBUTION: OK -----------t" << std::endl;
 			
-		}		
+		}		*/
 			
 				
 		//! New distribution data set
-		distribution_out.distri_vec.resize(1);	
+		distribution.distri_vec.resize(1);	
 	
 		//! Initialisation of runaway density
-		distribution_out.distri_vec(0).profiles_1d.state.dens.resize(N);
+		distribution.distri_vec(0).profiles_1d.state.dens.resize(N);
 	
 	
 		//! Initialisation of runaway current
-		distribution_out.distri_vec(0).profiles_1d.state.current.resize(N);
+		distribution.distri_vec(0).profiles_1d.state.current.resize(N);
 	
 		for (int i = 0; i < N; ++i){
 		
-			if (dist_initialised & flag==7){
+			/*if (dist_initialised & flag==7){
 			//! Filling up runaway density and current from the input distribution
-				distribution_out.distri_vec(0).profiles_1d.state.dens(i) = distribution_in.distri_vec(0).profiles_1d.state.dens(i);
-				distribution_out.distri_vec(0).profiles_1d.state.current(i) = distribution_in.distri_vec(0).profiles_1d.state.current(i);
+				distribution.distri_vec(0).profiles_1d.state.dens(i) = distribution_in.distri_vec(0).profiles_1d.state.dens(i);
+				distribution.distri_vec(0).profiles_1d.state.current(i) = distribution_in.distri_vec(0).profiles_1d.state.current(i);
 				
 				if(i==10) {std::cerr << "dens10prev: " << distribution_in.distri_vec(0).profiles_1d.state.dens(10) << std::endl;}
-			}else{	
+			}else{	*/
 			//! Filling up runaway density and current with zeroes
 			
-				distribution_out.distri_vec(0).profiles_1d.state.dens(i) = 0;
-				distribution_out.distri_vec(0).profiles_1d.state.current(i) = 0;
-			}	
+				distribution.distri_vec(0).profiles_1d.state.dens(i) = 0;
+				distribution.distri_vec(0).profiles_1d.state.current(i) = 0;
+			/*}	*/
 		}
 		
 		//! New distribution source
-		distribution_out.distri_vec(0).source_id.resize(1);	
+		distribution.distri_vec(0).source_id.resize(1);	
 	
 	
 		//! Filling up distribution source
-		distribution_out.distri_vec(0).source_id(0).type.id = "runaway";
-		distribution_out.distri_vec(0).source_id(0).type.flag = 7;
-		distribution_out.distri_vec(0).source_id(0).type.description = "Source from runaway processes";
+		distribution.distri_vec(0).source_id(0).type.id = "runaway";
+		distribution.distri_vec(0).source_id(0).type.flag = 7;
+		distribution.distri_vec(0).source_id(0).type.description = "Source from runaway processes";
 	
 	
 		//! Filling up distribution species
-		distribution_out.distri_vec(0).species.type.id = "electron";
-		distribution_out.distri_vec(0).species.type.flag = 1;
-		distribution_out.distri_vec(0).species.type.description = "Electron";
+		distribution.distri_vec(0).species.type.id = "electron";
+		distribution.distri_vec(0).species.type.flag = 1;
+		distribution.distri_vec(0).species.type.description = "Electron";
 	
 	
 		//! Filling up distribution gyro type
-		distribution_out.distri_vec(0).gyro_type = 1;
+		distribution.distri_vec(0).gyro_type = 1;
 	
 		//! Initialisation of geometry data		
-		distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor.resize(N);
-		distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor_norm.resize(N);
-		distribution_out.distri_vec(0).profiles_1d.geometry.psi(N);
+		distribution.distri_vec(0).profiles_1d.geometry.rho_tor.resize(N);
+		distribution.distri_vec(0).profiles_1d.geometry.rho_tor_norm.resize(N);
+		distribution.distri_vec(0).profiles_1d.geometry.psi(N);
 	
 	
 		//! Filling up runaway geometry data from the previous distribution
-		if (dist_initialised & flag==7){
+		/*if (dist_initialised & flag==7){
 		
 			for (int i = 0; i < N; ++i){
 				if (i < N_rho_tor){
-					distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor(i) = distribution_in.distri_vec(0).profiles_1d.geometry.rho_tor(i);
+					distribution.distri_vec(0).profiles_1d.geometry.rho_tor(i) = distribution_in.distri_vec(0).profiles_1d.geometry.rho_tor(i);
 				}
 		
 				if (i < N_rho_tor_norm){
-					distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i) = distribution_in.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i);
+					distribution.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i) = distribution_in.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i);
 				}			
 			}
 		
 		//! Filling up runaway geometry data from coreprof
-		}else{
+		}else{*/
 			
 			for (int i = 0; i < N; ++i){
 				if (i < N_rho_tor){
-					distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor(i) = coreprof.rho_tor(i);
+					distribution.distri_vec(0).profiles_1d.geometry.rho_tor(i) = coreprof.rho_tor(i);
 				}
 		
 				if (i < N_rho_tor_norm){
-					distribution_out.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i) = coreprof.rho_tor_norm(i);
+					distribution.distri_vec(0).profiles_1d.geometry.rho_tor_norm(i) = coreprof.rho_tor_norm(i);
 				}
 
 				/*if (i < N_psi){
-					distribution_out.distri_vec(0).profiles_1d.geometry.psi(i) = coreprof.psi.value(i);
+					distribution.distri_vec(0).profiles_1d.geometry.psi(i) = coreprof.psi.value(i);
 				}
 		
 				if (i < N_area){
-					distribution_out.distri_vec(0).profiles_1d.geometry.area(i) = coreimpur.area(i);
+					distribution.distri_vec(0).profiles_1d.geometry.area(i) = coreimpur.area(i);
 				}
 		
 				if (i < N_volume){
-					distribution_out.distri_vec(0).profiles_1d.geometry.volume(i) = coreimpur.volume(i);
+					distribution.distri_vec(0).profiles_1d.geometry.volume(i) = coreimpur.volume(i);
 				}*/
 			}
-		}
+		/*}*/
 		
 		
 		
@@ -155,14 +155,14 @@ void fire(ItmNs::Itm::distribution &distribution_in, ItmNs::Itm::distribution &d
 	*/
 	
 	
-		for (int i = 0; i < N; ++i){
+		/*for (int i = 0; i < N; ++i){
 		
 				if (dist_initialised & flag==7){
 				//! Filling up runaway density and current from the input distribution
 					distribution_temp.non_timed.float1d(0).value(i) = distribution_in.distri_vec(0).profiles_1d.state.dens(i);
 					//distribution_tempA.array(0).timed.float1d(0).value(i) = distribution_in.distri_vec(0).profiles_1d.state.dens(i);
 				}
-			}
+			}*/
 	
 	
 	
