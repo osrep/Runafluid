0c7bac5cbb1b6c2c962e9a1238ec721569fd9ae5
plasmatyi <aradimatyi@gmail.com>
2016-06-20 03:00:04 +0000

    [#924] Runafluid | cpo_to_profile | from distArray | bug fix

--- cpo_utils.cpp
+++ cpo_utils.cpp
@@ -259,8 +259,8 @@ profile cpo_to_profile(const ItmNs::Itm::coreprof &coreprof, const ItmNs::Itm::c
 		*/
 		
 		try{
-			/*celll.runaway_density = distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho);*/
-			celll.runaway_density = distribution.non_timed.float1d(0).value(rho);
+			celll.runaway_density = distribution.distri_vec(DISTSOURCE_IDENTIFIER).profiles_1d.state.dens(rho);
+			/*celll.runaway_density = distribution.non_timed.float1d(0).value(rho);*/
 
 
 		//! internal error in distribution
