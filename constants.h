ad72f9739fb14fe6db310a51ddc6c19599063fdf
gergopokol <gergopokol@gmail.com>
2016-07-07 10:36:11 +0000

    r1168 reverted in trunk/constants.h

--- constants.h
+++ constants.h
@@ -23,7 +23,7 @@
 #define ITM_EPS0	1.0 / (ITM_MU0 * ITM_C * ITM_C)
 
 //! distsource_identifier for runaways (7)
-#define DISTSOURCE_IDENTIFIER 7
+#define DISTSOURCE_IDENTIFIER 0
 
 //! ERROR CODE
 #define ITM_ILLEGAL_INT	-999999999
