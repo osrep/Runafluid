462bf3ca7eb3459710cf103544c41a6e80664028
plasmatyi <aradimatyi@gmail.com>
2016-09-15 15:41:34 +0000

    [#924] CRITICAL BUG FIX | non suitable warning: good result also for negative current

--- runafluid.cpp
+++ runafluid.cpp
@@ -198,7 +198,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		   	
 		   	//! not suitable warning: j_R > j_e	
 		   	ecurrent = it->electron_density * ITM_QE * ITM_C * sign(it->electric_field);
-		   	if (runcurrent >= ecurrent){		   	
+		   	if (runcurrent/ecurrent >= 1){		   	
 				not_suitable_warning = 1;	   	
 				//std::cerr << "[Runaway Fluid] Warning: Runaway current is higher than electron current (" << rho << ")" << std::endl;
 		   	}	   	
