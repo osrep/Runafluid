dd2d6645c2c980585bedc41d337da7fd69670ea2
plasmatyi <aradimatyi@gmail.com>
2016-09-15 12:24:27 +0000

    [#924] filling codeparam and output_flag

--- runafluid.cpp
+++ runafluid.cpp
@@ -118,6 +118,8 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 	//! stepping iterator in profile	
 	int rho = 0;	
 	
+	//! output flag
+	int output_flag = 0;
 	
 	//! start: runafluid
 	std::cerr << " START: runafluid" << std::endl;
@@ -218,16 +220,26 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		   	
    	//! error messages to dump
    	if (runaway_warning == 1){				
-			std::cerr << "[Runaway Fluid] Warning: Runaway electrons detected at " << time << " s" << std::endl;
+		std::cerr << "[Runaway Fluid] Warning: Runaway electrons detected at " << time << " s" << std::endl;
 	}		
 	
 	if (critical_fraction_warning == 1){				
 		std::cerr << "[Runaway Fluid] Warning: Runaway density is higher than the range of validity (critical fraction: " << critical_fraction << "%)  at " << time << " s" << std::endl;
+		output_flag -= 1;
 	}	
 	
 	if (not_suitable_warning == 1){				
 		std::cerr << "[Runaway Fluid] Warning: Runaway current is higher than electron current at " << time << " s" << std::endl;
+		output_flag -= 10;
 	}	
+	
+	//! output flag to distribution CPO
+	try {
+		distribution.codeparam.output_flag = output_flag;
+	} catch (const std::exception& ex) {
+		std::cerr << "[Runaway Fluid] ERROR: An error occurred during filling output_flag in codeparam" << std::endl;
+		std::cerr << "[Runaway Fluid] ERROR: " << ex.what() << std::endl;
+	}
 	
 	//! end: runafluid
 	std::cerr << " END: runafluid" << std::endl;
