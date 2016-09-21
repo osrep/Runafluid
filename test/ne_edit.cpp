0700d4d0e94ad25ccaaeff0b3aab405ee2304eb6
plasmatyi <aradimatyi@gmail.com>
2016-09-21 13:41:45 +0000

    [#924] start/end messages + code cleaning

--- test/ne_edit.cpp
+++ test/ne_edit.cpp
@@ -27,60 +27,13 @@ AB
 */
 
 
-/*!
-
-read_coreprof
-
-Reading coreprof to a profile. Other fields would be undefined!
-
-input: ItmNs::Itm::coreprof &coreprof
-output: profile
-*/
-
-/*
-profile read_coreprof(const ItmNs::Itm::coreprof &coreprof) {
-
-	profile pro;
-
-	//! read electron density profile length of dataset: cells	
-	int cells = coreprof.ne.value.rows();
-	
-	//! read electron temperature profile length of dataset, comparing with cells
-	if (coreprof.te.value.rows() != cells)
-		throw std::invalid_argument("Number of values is different in coreprof ne and te.");
-
-	//! read eparallel profile length of dataset, comparing with cells
-	if (coreprof.profiles1d.eparallel.value.rows() != cells)
-		throw std::invalid_argument(
-				"Number of values is different in coreprof.ne and coreprof.profiles1d.eparallel.");
-
-    //! read data in every $\rho$ 
-
-	for (int rho = 0; rho < cells; rho++) {
-		cell celll;
-				
-		//! electron density
-		celll.electron_density = coreprof.ne.value(rho);
-		
-		//! electron temperature
-		celll.electron_temperature = coreprof.te.value(rho);
-		
-	
-		celll.electric_field = coreprof.profiles1d.eparallel.value(rho); 
-
-		
-
-		pro.push_back(celll);
-	}
-
-	return pro;
-	
-}*/
-
-
 void fire(ItmNs::Itm::coreprof &coreprof, double &ne_value, int &ne_switch, double &output) {		
 		
 	try {
+		
+		
+		//! start: runafluid_neEdit
+		std::cerr << " START: runafluid_neEdit" << std::endl;
 		
 		bool bools[2];
 		
@@ -115,14 +68,18 @@ void fire(ItmNs::Itm::coreprof &coreprof, double &ne_value, int &ne_switch, doub
 		
 		}	
 		
-		output += (double)swint;		
+		output = 0;		
+		
+		
+		//! end: runafluid_neEdit
+		std::cerr << " END: runafluid_neEdit" << std::endl;
 
 	} catch (const std::exception& ex) {
-		std::cerr << "ERROR An error occurred during firing actor Runafluid." << std::endl;
-		std::cerr << "ERROR : " << ex.what() << std::endl;
-		output = ITM_ILLEGAL_INT;
-		
+	
 		//! internal error in distribution
+		std::cerr << "ERROR An error occurred during firing actor runafluid_neEdit." << std::endl;
+		std::cerr << "ERROR : " << ex.what() << std::endl;
+		output = ITM_ILLEGAL_INT;		
 		
 	}
 
--- test/te_edit.cpp
+++ test/te_edit.cpp
@@ -27,60 +27,12 @@ AB
 */
 
 
-/*!
-
-read_coreprof
-
-Reading coreprof to a profile. Other fields would be undefined!
-
-input: ItmNs::Itm::coreprof &coreprof
-output: profile
-*/
-/*
-profile read_coreprof(const ItmNs::Itm::coreprof &coreprof) {
-
-	profile pro;
-
-	//! read electron density profile length of dataset: cells	
-	int cells = coreprof.ne.value.rows();
-	
-	//! read electron temperature profile length of dataset, comparing with cells
-	if (coreprof.te.value.rows() != cells)
-		throw std::invalid_argument("Number of values is different in coreprof ne and te.");
-
-	//! read eparallel profile length of dataset, comparing with cells
-	if (coreprof.profiles1d.eparallel.value.rows() != cells)
-		throw std::invalid_argument(
-				"Number of values is different in coreprof.ne and coreprof.profiles1d.eparallel.");
-
-    //! read data in every $\rho$ 
-
-	for (int rho = 0; rho < cells; rho++) {
-		cell celll;
-				
-		//! electron density
-		celll.electron_density = coreprof.ne.value(rho);
-		
-		//! electron temperature
-		celll.electron_temperature = coreprof.te.value(rho);
-		
-	
-		celll.electric_field = coreprof.profiles1d.eparallel.value(rho); 
-
-		
-
-		pro.push_back(celll);
-	}
-
-	return pro;
-	
-}*/
-
-
 void fire(ItmNs::Itm::coreprof &coreprof, double &te_value, int &te_switch, double &output) {		
 		
 	try {
 		
+		//! start: runafluidteEdit
+		std::cerr << " START: runafluid_teEdit" << std::endl;
 		bool bools[2];
 		
 		int swint = int_switch(te_switch,bools,sizeof(bools)/sizeof(bool));		
@@ -114,15 +66,19 @@ void fire(ItmNs::Itm::coreprof &coreprof, double &te_value, int &te_switch, doub
 		
 		}	
 		
-		output += (double)swint;	
+		output = 0;	
+		
+		
+		//! end: runafluid_teEdit
+		std::cerr << " END: runafluid_teEdit" << std::endl;
 
 
 	} catch (const std::exception& ex) {
-		std::cerr << "ERROR An error occurred during firing actor Runafluid." << std::endl;
-		std::cerr << "ERROR : " << ex.what() << std::endl;
-		output = ITM_ILLEGAL_INT;
-		
+	
 		//! internal error in distribution
+		std::cerr << "ERROR An error occurred during firing actor runafluid_teEdit." << std::endl;
+		std::cerr << "ERROR : " << ex.what() << std::endl;
+		output = ITM_ILLEGAL_INT;		
 		
 	}
 
