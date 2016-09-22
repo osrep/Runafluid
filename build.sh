63d6d859c7052b289e53fa0fd04cf41706d7f334
plasmatyi <aradimatyi@gmail.com>
2016-09-22 12:11:47 +0000

    [#924] build.sh: fc2k implemented with code version and documentation

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/tcsh -fe
-make
+
+########## compiling code #########
+
+# with test files
+make t
+
+# without test files
+# make
+
+####### building FC2K files #######
+
+# runafluid (make)
+fc2k -docfile doc/runafluid.txt  -codeversion 1.0.1 fc2k/runaway_fluid.xml 
+
+# runafluid parameter editors (make t)
+fc2k test/fc2k/runafluid_efieldEdit.xml 
+fc2k test/fc2k/runafluid_neEdit.xml 
+fc2k test/fc2k/runafluid_teEdit.xml 
