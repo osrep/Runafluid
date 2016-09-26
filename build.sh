7043a9ee785d927f423e36a36317dbd2039a9d03
plasmatyi <aradimatyi@gmail.com>
2016-09-26 16:05:16 +0000

    [#924] build.sh: version number updated

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ make
 ####### building FC2K files #######
 
 # runafluid (make)
-fc2k -docfile doc/runafluid.txt -codeversion 1.1.0 fc2k/runafluid.xml 
-fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.1.0 fc2k/runafluid_efieldEdit.xml 
+fc2k -docfile doc/runafluid.txt -codeversion 1.0.0 fc2k/runafluid.xml 
+fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.0.0 fc2k/runafluid_efieldEdit.xml 
 
 # runafluid parameter editors (make t)
 # fc2k test/fc2k/runafluid_neEdit.xml 
