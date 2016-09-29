ad152326c01711248b6b60a2dabbd8f8f63f3a5a
plasmatyi <aradimatyi@gmail.com>
2016-09-29 09:07:34 +0000

    [#924] build.sh: new policy for build.sh (fc2k files in run_fc2k.sh)

---
+++ run_fc2k.sh
@@ -0,0 +1,11 @@
+#!/bin/tcsh
+
+####### building FC2K files #######
+
+# runafluid (make)
+fc2k -docfile doc/runafluid.txt -codeversion 1.1.0 fc2k/runafluid.xml 
+fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.1.0 test/fc2k/runafluid_efieldEdit.xml 
+
+# runafluid parameter editors (make t)
+# fc2k test/fc2k/runafluid_neEdit.xml 
+# fc2k test/fc2k/runafluid_teEdit.xml 
