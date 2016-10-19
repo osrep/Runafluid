e641c08025cbd7040a6cb56c6ddc145c2cfb61ca
plasmatyi <aradimatyi@gmail.com>
2016-10-19 16:00:21 +0000

    [#924] fc2k path updated

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -3,7 +3,7 @@
 ####### building FC2K files #######
 
 # runafluid (make)
-fc2k -docfile doc/runafluid.txt -codeversion 1.1.0 fc2k/runafluid.xml 
+fc2k -docfile doc/runaway_fluid.txt -codeversion 1.1.0 fc2k/runaway_fluid.xml 
 fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.1.0 fc2k/runafluid_efieldEdit.xml 
 
 
