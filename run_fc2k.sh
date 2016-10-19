283b311e8dfbb3e3467afb35ca0594dee7d8d125
plasmatyi <aradimatyi@gmail.com>
2016-10-19 15:49:21 +0000

    [#924] test builder added

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -6,6 +6,4 @@
 fc2k -docfile doc/runafluid.txt -codeversion 1.1.0 fc2k/runafluid.xml 
 fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.1.0 fc2k/runafluid_efieldEdit.xml 
 
-# runafluid parameter editors (make t)
-# fc2k test/fc2k/runafluid_neEdit.xml 
-# fc2k test/fc2k/runafluid_teEdit.xml 
+
---
+++ test/build_test.sh
@@ -0,0 +1,8 @@
+# runafluid parameter editors (make t)
+
+cd ..
+
+make t
+
+fc2k test/fc2k/runafluid_neEdit.xml 
+fc2k test/fc2k/runafluid_teEdit.xml 
