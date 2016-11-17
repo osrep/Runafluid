2d9a9d8c59716d7b0c308fcec55fd729c5b30d48
plasmatyi <aradimatyi@gmail.com>
2016-11-17 17:30:36 +0000

    [#924] Runafluid test workflow plotting python script | triple plot added

--- workflow/plotter.py
+++ workflow/plotter.py
@@ -7,14 +7,25 @@ runaway_current = distribution0[0].distri_vec[0].profiles_1d.state.current
 fig1=plt.figure(1)
 plt.plot(rho_tor_norm, runaway_density)
 plt.xlabel('normalised rho tor')
-plt.ylabel('runaway density [1/m^3] ')
+plt.ylabel('runaway density [1/m3$] ')
 fig1.show()
 
 
 fig2=plt.figure(2)
 plt.plot(rho_tor_norm, runaway_current)
 plt.xlabel('normalised rho tor')
-plt.ylabel('runaway current [A/m^3] ')
+plt.ylabel('runaway current [A/m3] ')
 fig2.show()
+
+# Three subplots sharing both x/y axes
+fig3, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+ax1.plot(rho_tor_norm, runaway_density)
+ax1.set_title('Sharing both axes')
+ax2.scatterrho_tor_norm, runaway_density)
+ax3.scatterrho_tor_norm, runaway_density, color='r')
+# Fine-tune figure; make subplots close to each other and hide x ticks for
+# all but bottom plot.
+fig3.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in fig3.axes[:-1]], visible=False)
 
 raw_input()
