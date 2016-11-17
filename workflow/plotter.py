c22f90d6e566f63f44627b42dddc10ba49290b57
plasmatyi <aradimatyi@gmail.com>
2016-11-17 17:17:34 +0000

    [#924] Runafluid test workflow plotting python script updated

--- workflow/plotter.py
+++ workflow/plotter.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
 
-
 rho_tor_norm = distribution0[0].distri_vec[0].profiles_1d.geometry.rho_tor_norm
 runaway_density = distribution0[0].distri_vec[0].profiles_1d.state.dens
-
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.plot(rho_tor_norm, runaway_density)
-ax1.xlabel('normalised rho tor')
-ax1.ylabel('runaway density [1/m3] ')
-ax1.show()
+plt.figure(1)
+plt.plot(rho_tor_norm, runaway_density)
+plt.xlabel('normalised rho tor')
+plt.ylabel('runaway density [1/m3] ')
+plt.show()
