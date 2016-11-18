ddb2174a5c8bdac70bff51ea9b2faaf14bf5926c
plasmatyi <aradimatyi@gmail.com>
2016-11-18 06:02:34 +0000

    [#924] Python plotter | Avalanche added

--- workflow/plotter.py
+++ workflow/plotter.py
@@ -10,6 +10,13 @@ dreicer_rate66 = temporary0[0].timed.float1d[3].value
 dreicer_rate67 = temporary0[0].timed.float1d[4].value
 
 
+avalanche_rate = temporary0[0].timed.float1d[1].value
+avalanche_rate_lin = temporary0[0].timed.float1d[12].value
+avalanche_rate_thr = temporary0[0].timed.float1d[13].value
+
+rel_efield = temporary0[0].timed.float1d[19].value
+
+
 fig1=plt.figure(1)
 plt.plot(rho_tor_norm, runaway_density)
 plt.xlabel('normalised rho tor')
@@ -46,7 +53,36 @@ plt.legend(loc=2)
 fig4.show()
 fig4.savefig('/u/maradi/svn/runafluid/trunk/workflow/plot_dreicer_all.pdf')
 
+fig5=plt.figure(5)
+plt.plot(rho_tor_norm, dreicer_rate63, label='H&C (63) without toroidicity')
+plt.plot(rho_tor_norm, dreicer_rate, label='H&C (63)  with toroidicity')
+plt.xlabel('normalised rho tor')
+plt.ylabel('Dreicer rate [1/m3s] ')
+plt.legend(loc=2)
+fig5.show()
+fig5.savefig('/u/maradi/svn/runafluid/trunk/workflow/plot_dreicer_tor.pdf')
 
+
+
+fig7=plt.figure(7)
+plt.plot(rho_tor_norm, avalanche_rate_lin, label='Rosenbluth-Putvinski linear model')
+plt.plot(rho_tor_norm, avalanche_rate_thr, label='Onset threshold model')
+plt.xlabel('normalised rho tor')
+plt.ylabel('normalised Avalanche rate [1/s]')
+plt.legend(loc=2)
+fig7.show()
+fig7.savefig('/u/maradi/svn/runafluid/trunk/workflow/plot_avalanche_threshold.pdf')
+
+
+
+fig8=plt.figure(8)
+plt.plot(rho_tor_norm, avalanche_rate_thr, label='OTM without toroidicity')
+plt.plot(rho_tor_norm, avalanche_rate, label='OTM  with toroidicity')
+plt.xlabel('normalised rho tor')
+plt.ylabel('normalised Avalanche rate [1/s]')
+plt.legend(loc=2)
+fig8.show()
+fig8.savefig('/u/maradi/svn/runafluid/trunk/workflow/plot_avalanche_tor.pdf')
 
 # Three subplots sharing both x/y axes
 #fig3, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
