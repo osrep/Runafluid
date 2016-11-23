00a852953699cc36e4114a0d9b03ecbaa0d221d3
plasmatyi <aradimatyi@gmail.com>
2016-11-23 15:27:49 +0000

    HDF saver Fortran rutin | update | first working version

--- test/hdf5save.f90
+++ test/hdf5save.f90
@@ -1,23 +1,58 @@
 program diagnostic
+
     use euitm_schemas
     use euitm_routines
     use write_structures
     implicit none
 
-    integer :: idx
-    type (type_equilibrium), pointer :: equilibrium(:)
+  TYPE (TYPE_COREPROF), pointer :: COREPROF(:)
+  TYPE (TYPE_EQUILIBRIUM), pointer :: EQUILIBRIUM(:)
+  TYPE (TYPE_CORESOURCE), pointer :: CORESOURCE(:)
+  TYPE (TYPE_CORETRANSP), pointer :: CORETRANSP(:)
+  TYPE (TYPE_EDGE), pointer :: EDGE(:)
+  TYPE (TYPE_DISTRIBUTION), pointer :: DISTRIBUTION(:)
+  integer idx, idx3, i, cposize
+
+
+    integer :: idx2
+    type (type_equilibrium), pointer :: equilibrium2(:)
 
     Integer x
 
-    call euitm_open('euitm', 12, 2, idx)
+    call euitm_open('euitm', 28906, 7, idx2)
+    
+    call euitm_get(idx2, 'equilibrium', equilibrium2)
 
-    call euitm_get(idx, 'equilibrium', equilibrium)
+    write (*,*) "Size of CPO: ", size(equilibrium2)
 
-    write (*,*) "Size of CPO: ", size(equilibrium)
+    write (*,*) "Value of r: ", equilibrium2(1)%eqgeometry%boundary(1)%r(6)
+    write (*,*) "Value of z: ", equilibrium2(1)%eqgeometry%boundary(1)%z(6)
 
-    write (*,*) "Value of r: ", equilibrium(1)%eqgeometry%boundary(1)%r(1)
-    write (*,*) "Value of z: ", equilibrium(1)%eqgeometry%boundary(1)%z(1)
+    call euitm_close(idx2)
 
-    call euitm_close(idx)
-    call euitm_disconnect()
+
+  write(*,*) 'Reading data'
+  call euitm_open('euitm',28906, 1007,idx)
+
+  call euitm_get(idx, 'equilibrium', equilibrium)
+  cposize=size(equilibrium)
+  call euitm_get(idx, 'coreprof', coreprof)
+  call euitm_get(idx, 'distribution', distribution)
+  !call euitm_get(idx, 'coresource', coresource)
+  !call euitm_get(idx, 'coretransp', coretransp)
+  !call euitm_get(idx, 'edge', edge)
+  call euitm_close(idx)
+  write(*,*) 'Read data'
+
+  call euitm_create_hdf5('euitm',9,9998,0,0,idx3)
+  call euitm_put(idx3,"coreprof",coreprof)
+  call euitm_put(idx3,"equilibrium",equilibrium)
+  call euitm_put(idx3,"distribution",distribution)
+
+
+  call euitm_close(idx3)
+
+
+
 end program
+
