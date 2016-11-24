a96967c223d99f4d21741040791d82bfb8334e87
plasmatyi <aradimatyi@gmail.com>
2016-11-24 15:58:21 +0000

    HDF saver Fortran rutin | hdfsave env. variable set added | bug fix

--- test/hdf5/setenv.sh
+++ test/hdf5/setenv.sh
@@ -1,6 +1,7 @@
 #!/bin/tcsh -fe
 
-if (setenv euitm_path $MDSPLUS_TREE_BASE_0) then
+if ($?MDSPLUS_TREE_BASE_0) then
+	setenv euitm_path $MDSPLUS_TREE_BASE_0
 	echo "euitm_path set to "+$euitm_path 
 else
 	setenv $MDSPLUS_TREE_BASE_0 $HOME/public/itmdb/itm_trees/aug/4.10b/hdf5
