f7838b7d345cd7f4779125c8971a239002994a24
plasmatyi <aradimatyi@gmail.com>
2017-04-25 16:33:23 +0000

    [#1176] Python and own Fortran modules added

--- test/expdecay.f90
+++ test/expdecay.f90
@@ -6,3 +6,6 @@ integer,parameter :: DP=kind(1.0D0)
 type (type_coreprof),pointer :: coreprof(:)
 type (type_coresource),pointer :: coresource(:)
 real(DP),pointer :: time(:)
+
+return
+end subroutine
