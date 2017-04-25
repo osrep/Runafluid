8d614060a32ac77166f352c48f64787b45b0ab20
plasmatyi <aradimatyi@gmail.com>
2017-04-25 17:07:29 +0000

    [#1176] Python and own Fortran modules added

--- test/expdecay.f90
+++ test/expdecay.f90
@@ -1,11 +1,15 @@
-subroutine expdecay(coreprof,coresource,time)
+subroutine expdecay(coreprof,equilibrium,coresource)
 
 use euitm_schemas   !use euITM_routines ! Needed only if using euitm_deallocate and euitm_copy functions
 implicit none
 integer,parameter :: DP=kind(1.0D0)
 type (type_coreprof),pointer :: coreprof(:)
+type (type_equilibrium),pointer :: coreprof(:)
 type (type_coresource),pointer :: coresource(:)
 real(DP),pointer :: time(:)
 
 return
 end subroutine
+
+
+
