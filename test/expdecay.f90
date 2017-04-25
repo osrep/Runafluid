2e1747b1592fc670a30721b5afb3b8eaec9f63ee
plasmatyi <aradimatyi@gmail.com>
2017-04-25 20:15:55 +0000

    [#1176] time removed (bug fix) + use allocate_deallucate added

--- test/expdecay.f90
+++ test/expdecay.f90
@@ -1,12 +1,12 @@
 subroutine expdecay(in_coreprof,in_equilibrium,out_coresource)
 
 use euitm_schemas   !use euITM_routines ! Needed only if using euitm_deallocate and euitm_copy functions
+use allocate_deallocate
 implicit none
 integer,parameter :: DP=kind(1.0D0)
 type (type_coreprof),pointer :: in_coreprof(:)
 type (type_equilibrium),pointer :: in_equilibrium(:)
 type (type_coresource),pointer :: out_coresource(:)
-real(DP),pointer :: time(:)
 allocate(out_coresource(1))
 allocate(out_coresource(1)%codeparam%codename(1))   ! For a string of 132 characters max.
 out_coresource(1)%codeparam%codename(1)   = 'expdecay'
