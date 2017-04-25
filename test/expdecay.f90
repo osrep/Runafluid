4c094af771a42a3b436f355e1d1db074c15caa5f
plasmatyi <aradimatyi@gmail.com>
2017-04-25 17:08:23 +0000

    [#1176] Python and own Fortran modules added

--- test/expdecay.f90
+++ test/expdecay.f90
@@ -4,7 +4,7 @@ use euitm_schemas   !use euITM_routines ! Needed only if using euitm_deallocate 
 implicit none
 integer,parameter :: DP=kind(1.0D0)
 type (type_coreprof),pointer :: coreprof(:)
-type (type_equilibrium),pointer :: coreprof(:)
+type (type_equilibrium),pointer :: equilibrium(:)
 type (type_coresource),pointer :: coresource(:)
 real(DP),pointer :: time(:)
 
