474bba95156cddb8613acdb6fbca0a40a008605a
plasmatyi <aradimatyi@gmail.com>
2017-05-08 10:29:39 +0000

    [#1176] New stable init&alloc for python

--- test/expdecay.py
+++ test/expdecay.py
@@ -1,11 +1,28 @@
-coresource1.time = coresource0.time 
-coresource1.values.rho_tor = coresource0.values.rho_tor 
-coresource1.values.j = coresource0.values.j  
-coresource1.values.qe.exp = coresource0.values.qe.exp 
-coresource1.values.qe.imp = coresource0.values.qe.imp 
-coresource1.values.se.exp = coresource0.values.se.exp 
-coresource1.values.se.imp = coresource0.values.se.imp 
-#coresource1.values. = coresource0.values.  
-coresource1.values.sourceid.id = 'expdecay" 
-coresource1.values.sourceid.flag = coresource0.values.sourceid.flag  
-coresource1.values.sourceid.description = 'Exponential heat drain'
+import ual
+import numpy
+
+# Get rho length
+#rho_size = len(coreprof0(0).rho_tor)
+
+# Allocate coresource
+coresource0 = ual.coresource.coresourceArray()
+coresource0.resize(1)
+##coresource0.resize(len(coreprof0.array))
+#coresource0.values.resize(1)
+
+
+# Source description
+#coresource0.values(0).sourceid.id = 'expdecay" 
+#coresource0.values(0).sourceid.flag = coresource0.values.sourceid.flag  
+#coresource0.values(0).sourceid.description = 'Exponential heat drain'
+
+# Filling up data
+
+#for i in range(0, len(coreprof0.rho_tor)-1)
+#   coresource0.values(0).rho_tor(i) = coreprof0.rho_tor(i) 
+#   coresource0.values(0).j(i) = 0
+#   coresource0.values(0).qe.exp(i) = 0
+#   coresource0.values(0).qe.imp(i) = coreprof0.te/1.6e-19 # hack
+#   coresource0.values(0).se.exp(i) = 0
+#   coresource0.values(0).se.imp(i) = 0
+
