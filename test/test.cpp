08bf28d5b44f39085d605460273ec56ae3f7163c
Hadaradam <hadaradam20@gmail.com>
2017-05-11 13:39:49 +0000

    added tests for calculator functions

--- test/test.cpp
+++ test/test.cpp
@@ -13,8 +13,23 @@ const double reference_thermal_electron_collision_time = 4.9909e-4;
 const double reference_runaway_electron_collision_time = 2.0383e-3;
 const double reference_Zeff_1 = 1.0;
 const double reference_Zeff_2 = 1.2;
-const double reference_electric_field_1 = 1.0;
-const double reference_electric_field_2 = 1.2;
+const double reference_electric_field_1 = 1.2;
+const double reference_electric_field_2 = 0.8;
+
+const int formula_id_66 = 63;
+const int formula_id_66 = 66;
+const int formula_id_67 = 67;
+const double reference_dreicer_generation_rate_63 = 3.0822e20;
+const double reference_dreicer_generation_rate_66 = 5.3092e19;
+const double reference_dreicer_generation_rate_67 = 1.2513e15;
+const double reference_alpha = reference_electric_field_1;
+const double reference_lambda = 2.0169;
+const double reference_gamma =0.6173;
+const double reference_h = 1.8865;
+
+const double reference_inv_asp_ratio = 0.30303;
+const double reference_toroidicity_dreicer = 0.1816067;
+
 
 TEST(Equal, Tolerance) {
 	EXPECT_TRUE(equal(1.0, 1.1, 0.1));
@@ -215,36 +230,43 @@ TEST(GrowthRate, CalculateDreicerField) {
 EXPECT_NEAR(reference_dreicer_field, calculate_dreicer_field(reference_thermal_electron_collision_time, reference_te), 0.01);
 }
 
-
 TEST(GrowthRate, CalculateThermalElectronCollisionTime) {
 EXPECT_NEAR(reference_thermal_electron_collision_time, calculate_thermal_electron_collision_time(reference_ne,reference_te), 0.0001);
 }
 
-TEST(GrowthRate, CalculateGrowthRate_1) {
-	EXPECT_NEAR(reference_growth_rate_1, calculate_growth_rate(reference_ne, reference_te, reference_Zeff_1, reference_electric_field_1), 1e18);
+
+
+TEST(Dreicer, DreicerGenerationRate_63) {
+EXPECT_NEAR(reference_dreicer_generation_rate_63, dreicer_generation_rate(reference_ne, reference_te,
+																		  reference_Zeff_1,reference_electric_field_1,formula_id_63), 1e18);
 }
 
-TEST(GrowthRate, CalculateGrowthRate_2) {
-EXPECT_NEAR(reference_growth_rate_2, calculate_growth_rate(reference_ne, reference_te, reference_Zeff_2, reference_electric_field_2), 1e18);
+TEST(Dreicer, DreicerGenerationRate_66) {
+EXPECT_NEAR(reference_dreicer_generation_rate_66, dreicer_generation_rate(reference_ne, reference_te,
+																	   reference_Zeff_1,reference_electric_field_1,formula_id_66), 1e18);
 }
 
-TEST(GrowthRate, IsGrowthRateOverLimit) {
-	cell cell1, cell2;
+TEST(Dreicer, DreicerGenerationRate_67) {
+EXPECT_NEAR(reference_dreicer_generation_rate_67, dreicer_generation_rate(reference_ne, reference_te,
+																	   reference_Zeff_1,reference_electric_field_1,formula_id_67), 1e18);
+}
 
-	cell1.electron_density = 0.9*reference_ne;
-	cell1.electron_temperature = reference_te;
-	cell1.effective_charge = reference_Zeff_1;
-	cell1.electric_field = reference_electric_field_1;
+TEST(Dreicer, CalculateAlpha){
+	EXPECT_EQ(reference_alpha,calculate_alpha(reference_electric_field_1, reference_ne,reference_te));
+}
 
-	cell2.electron_density = 1.1*reference_ne;
-	cell2.electron_temperature = reference_te;
-	cell2.effective_charge = reference_Zeff_2;
-	cell2.electric_field = reference_electric_field_2;
+TEST(Dreicer, CalculateLambda){
+EXPECT_EQ(reference_lambda,calculate_lambda(reference_alpha));
+}
 
-	profile pro;
-	pro.push_back(cell1);
-	EXPECT_EQ(1, is_growth_rate_over_limit(pro, reference_growth_rate_1));
+TEST(Dreicer, CalculateGamma){
+EXPECT_EQ(reference_gamma,calculate_gamma(reference_Zeff_1, reference_alpha)));
+}
 
-	pro.push_back(cell2);
-	EXPECT_EQ(0, is_growth_rate_over_limit(pro, reference_growth_rate_2));
+TEST(Dreicer, CalculateH){
+EXPECT_EQ(reference_h,calculate_h(reference_alpha, reference_Zeff_1));
+}
+
+TEST(Dreicer, CalculateToroidicityDreicer){
+EXPECT_NEAR(reference_toroidicity_dreicer,calculate_toroidicity_dreicer(reference_inv_asp_ratio),1e18);
 }
