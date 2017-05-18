e3e22a43fa89007576df62d83d10b8785294088f
plasmatyi <aradimatyi@gmail.com>
2017-05-18 14:28:56 +0000

    [#1176] explicit added to fc2k

--- test/build_test.sh
+++ test/build_test.sh
@@ -4,6 +4,7 @@ cd ..
 
 make dev
 
-fc2k test/fc2k/runafluid_neEdit.xml 
-fc2k test/fc2k/runafluid_teEdit.xml 
-fc2k test/fc2k/runafluid_qeimpEdit.xml
+#fc2k test/fc2k/runafluid_neEdit.xml 
+#fc2k test/fc2k/runafluid_teEdit.xml 
+#fc2k test/fc2k/runafluid_qeimpEdit.xml
+fc2k test/fc2k/runafluid_qeexpEdit.xml
