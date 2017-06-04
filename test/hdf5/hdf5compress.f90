28b45138206d9b63c5d31f0cd306d16a1e476212
plasmatyi <aradimatyi@gmail.com>
2017-06-04 02:06:27 +0000

    [#1176] hdf5 compresser | rho_tor, q, zeff, r0 added

--- test/hdf5/hdf5compress.f90
+++ test/hdf5/hdf5compress.f90
@@ -60,9 +60,18 @@ program hdf5compress
 	rho_length = size(coreprof_in(1)%rho_tor_norm)
 	
 	do i=1,coreprof_slices
+		coreprof_out(i)%toroid_field%r0 = coreprof_in(i)%toroid_field%r0
+		
+		allocate(coreprof_out(i)%rho_tor(rho_length))
 		allocate(coreprof_out(i)%rho_tor_norm(rho_length))
+		allocate(coreprof_out(i)%profiles1d%q%value(rho_length))
+		allocate(coreprof_out(i)%profiles1d%zeff%value(rho_length))
+				
 		do j=1,rho_length
-			coreprof_out(i)%rho_tor_norm(j) = coreprof_in(i)%rho_tor_norm(j)	
+			coreprof_out(i)%rho_tor(j) = coreprof_in(i)%rho_tor(j)
+			coreprof_out(i)%rho_tor_norm(j) = coreprof_in(i)%rho_tor_norm(j)
+			coreprof_out(i)%profiles1d%q%value(j) = coreprof_in(i)%profiles1d%q%value(j)
+			coreprof_out(i)%profiles1d%zeff%value(j) = coreprof_in(i)%profiles1d%zeff%value(j)
 		end do	
 	end do
 
