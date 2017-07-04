2c16a7148f924fd27ffd7cfedf588a22b2146520
plasmatyi <aradimatyi@gmail.com>
2017-07-04 13:14:28 +0000

    [#1203] IMAS headers updated

--- constants.h
+++ constants.h
@@ -1,40 +1,40 @@
 #ifndef CONSTANTS_H_
-#define CONSTANTS_H_
+    #define CONSTANTS_H_
 
-#ifdef ITM_CONSTANTS
-    #include <itm_constants.h>
-    
-#elif IMAS_CONSTANTS
-    #include <imas_constants.h>
-    
-#else
-    //! value of \f$ \pi \f$
-    #define ITM_PI		3.141592653589793238462643383280
+    #ifdef ITM_CONSTANTS
+        #include <itm_constants.h>
+        
+    #elif IMAS_CONSTANTS
+        #include <imas_constants.h>
+        
+    #else
+        //! value of \f$ \pi \f$
+        #define ITM_PI		3.141592653589793238462643383280
 
-    //! speed of light [m/s]
-    #define ITM_C		2.99792458e8
+        //! speed of light [m/s]
+        #define ITM_C		2.99792458e8
 
-    //! electron volt [eV]
-    #define ITM_EV		1.602176565e-19
+        //! electron volt [eV]
+        #define ITM_EV		1.602176565e-19
 
-    //! elementary charge [coulomb]
-    #define ITM_QE		ITM_EV
+        //! elementary charge [coulomb]
+        #define ITM_QE		ITM_EV
 
-    //! \f$ m_e \f$ electron mass [kg]
-    #define ITM_ME		9.10938291e-31
+        //! \f$ m_e \f$ electron mass [kg]
+        #define ITM_ME		9.10938291e-31
 
-    //! \f$ \mu_0 \f$ vacuum permeability
-    #define ITM_MU0		4.0e-7 * ITM_PI
+        //! \f$ \mu_0 \f$ vacuum permeability
+        #define ITM_MU0		4.0e-7 * ITM_PI
 
-    //! \f$ \epsilon_0 \f$ vacuum permittivity
-    #define ITM_EPS0	1.0 / (ITM_MU0 * ITM_C * ITM_C)
+        //! \f$ \epsilon_0 \f$ vacuum permittivity
+        #define ITM_EPS0	1.0 / (ITM_MU0 * ITM_C * ITM_C)
 
-    //! ERROR CODE
-    #define ITM_INVALID_INT	-999999999
+        //! ERROR CODE
+        #define ITM_INVALID_INT	-999999999
 
-#endif
+    #endif
 
-//! distsource_identifier for runaways (7)
-const int DISTSOURCE_IDENTIFIER = 7;
+    //! distsource_identifier for runaways (7)
+    const int DISTSOURCE_IDENTIFIER = 7;
 
 #endif /* CONSTANTS_H_ */
--- distinit.h
+++ distinit.h
@@ -1,7 +1,10 @@
 #ifndef DISTINIT_H_
-#define DISTINIT_H_
+    #define DISTINIT_H_
 
-void distinit(ItmNs::Itm::distribution &distribution, ItmNs::Itm::coreprof &coreprof);
-
-
+    #ifdef ITM_CONSTANTS    
+        void distinit(ItmNs::Itm::distribution &distribution, ItmNs::Itm::coreprof &coreprof);        
+    #elif IMAS_CONSTANTS    
+        void distinit(const IdsNs::IDS::distribution &distribution, const IdsNs::IDS::core_profiles &core_profiles);
+    #endif
+    
 #endif /* DISTINIT_H_ */
