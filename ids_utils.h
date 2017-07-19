3f236a4d5797909c38fd690e196ddd96b55959af
plasmatyi <aradimatyi@gmail.com>
2017-07-19 12:18:37 +0000

    [#1202] IDS handling set up | bug fix

--- ids_utils.h
+++ ids_utils.h
@@ -2,13 +2,17 @@
 #define IDS_UTILS_H_
 
 #include <UALClasses.h>
-#include "cell.h"
+#include "constants.h"
+#include "control.h"
 
 bool equal(double a, double b, double tolerance);
 
 int binary_search(const Array<double, 1> &array, double search_key);
 
 double interpolate(const Array<double, 1> &x, const Array<double, 1> &y, double xa);
+
+
+int whereRunaway(IdsNs::IDS::distributions &distributions);
 		
 profile ids_to_profile(const IdsNs::IDS::core_profiles &core_profiles, const IdsNs::IDS::equilibrium &equilibrium, const IdsNs::IDS::distributions &distributions, int timeindex);
 
