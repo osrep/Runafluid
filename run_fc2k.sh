7339b1dd280ea3dfe80cf08f4a22245481b377a2
plasmatyi <aradimatyi@gmail.com>
2017-07-19 14:54:21 +0000

    version number updated

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -3,7 +3,7 @@
 ####### building FC2K files #######
 
 # runafluid (make)
-fc2k -docfile doc/runaway_fluid.txt -codeversion 1.6.2 fc2k/runaway_fluid.xml 
-fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.6.2 fc2k/runafluid_efieldEdit.xml 
+fc2k -docfile doc/runaway_fluid.txt -codeversion 1.3.1 fc2k/runaway_fluid.xml 
+fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.3.1 fc2k/runafluid_efieldEdit.xml 
 
 
