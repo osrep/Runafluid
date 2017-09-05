b5447835b33a2d8e5f9325e1ff2b8f2f4279142c
plasmatyi <aradimatyi@gmail.com>
2017-09-05 09:55:44 +0000

    [#1202] fc2k support added to IMAS infrastructure

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -3,7 +3,21 @@
 ####### building FC2K files #######
 
 # runafluid (make)
-fc2k -docfile doc/runaway_fluid.txt -codeversion 1.3.1 fc2k/runaway_fluid.xml 
-fc2k -docfile doc/runafluid_efieldEdit.txt -codeversion 1.3.1 fc2k/runafluid_efieldEdit.xml 
 
+# runaway indicator (EU-IM)
 
+if ( ( $?ITM_ENVIRONMENT_LOADED) )  then
+    if ( $ITM_ENVIRONMENT_LOADED == yes)  then
+        echo "*** Compiler set to ITM ***"
+        rmactor runaway_fluid
+        rmactor runafluid_efieldEdit
+        fc2k -docfile doc/runaway_fluid.txt fc2k/runaway_fluid.xml 
+        fc2k -docfile doc/runafluid_efieldEdit.txt fc2k/runafluid_efieldEdit.xml 
+    else
+        echo "*** Compiler set to IMAS ***"
+        rmactor runaway_fluid
+        rmactor runafluid_efieldEdit
+        fc2k -docfile doc/runaway_fluid.txt fc2k_imas/runaway_fluid.xml 
+        fc2k -docfile doc/runafluid_efieldEdit.txt fc2k_imas/runafluid_efieldEdit.xml 
+    endif
+endif
