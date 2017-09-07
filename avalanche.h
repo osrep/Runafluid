c6901e1aa0a86e1b2777c077e20cdad930005dd1
plasmatyi <aradimatyi@gmail.com>
2017-09-07 12:49:08 +0000

    [#1202] Avalanche header bug fix

--- avalanche.h
+++ avalanche.h
@@ -5,8 +5,8 @@ double avalanche_generation_rate(double electron_density, double electron_temper
 double calculate_toroidicity_avalanche(double inv_asp_ratio, double electric_field, double electron_density, double electron_temperature, double rho_tor_norm);
 double calculate_avalanche_threshold_field(double electron_density, double electron_temperature, double effective_charge, double critical_field);
 
+#endif /* AVALANCHE_H_ */
 
-)/*double calculate_flow_Ap(double p, double E, double Z);
+/*double calculate_flow_Ap(double p, double E, double Z);
 double calculate_flow_costheta(double p, double E, double Z);
 double calculate_flow_velocity(double electron_density, double electron_temperature, double effective_charge, double electric_field, double magnetic_field);*/
-#endif /* AVALANCHE_H_ */
