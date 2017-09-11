f256bf7f7b9aafe06ab461ec123ffdad6ebf0115
plasmatyi <aradimatyi@gmail.com>
2017-09-11 16:15:41 +0000

    [#1273] First try of code parameters

--- runafluid.cpp
+++ runafluid.cpp
@@ -94,7 +94,7 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 		  ItmNs::Itm::equilibrium &equilibrium, ItmNs::Itm::distribution &distribution_in,
 		  ItmNs::Itm::distribution &distribution_out, double &timestep, int &runafluid_switch,
 		  double &critical_fraction, int &runaway_warning, int &not_suitable_warning, int &critical_fraction_warning,
-		  ItmNs::Itm::temporary &runaway_rates) {
+		  ItmNs::Itm::temporary &runaway_rates, ItmNs::codeparam_t&) {
 
 	//! start: runafluid
 	std::cerr << " START: runaway_fluid" << std::endl;
