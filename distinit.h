fd313a6eca717646b6ad5b5614ee7cca850d4765
plasmatyi <aradimatyi@gmail.com>
2017-09-14 02:58:16 +0000

    [#1202] distinit updated for IMAS infrastructure | bug fix: const removed

--- distinit.h
+++ distinit.h
@@ -4,7 +4,7 @@
     #ifdef ITM_CONSTANTS    
         void distinit(ItmNs::Itm::distribution &distribution, ItmNs::Itm::coreprof &coreprof);        
     #else   
-        void distinit(const IdsNs::IDS::distribution &distribution, const IdsNs::IDS::core_profiles &core_profiles, int timeindex);
+        void distinit(IdsNs::IDS::distribution &distribution, IdsNs::IDS::core_profiles &core_profiles, int timeindex);
     #endif
     
 #endif /* DISTINIT_H_ */
