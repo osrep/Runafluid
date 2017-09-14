0503c9a49e2cd959d38511ea47a315bbae05ccae
plasmatyi <aradimatyi@gmail.com>
2017-09-14 03:01:09 +0000

    [#1202] distinit updated for IMAS infrastructure | bug fix: disribution->distributions

--- distinit.h
+++ distinit.h
@@ -4,7 +4,7 @@
     #ifdef ITM_CONSTANTS    
         void distinit(ItmNs::Itm::distribution &distribution, ItmNs::Itm::coreprof &coreprof);        
     #else   
-        void distinit(IdsNs::IDS::distribution &distribution, IdsNs::IDS::core_profiles &core_profiles, int timeindex);
+        void distinit(IdsNs::IDS::distributions &distributions, IdsNs::IDS::core_profiles &core_profiles, int timeindex);
     #endif
     
 #endif /* DISTINIT_H_ */
