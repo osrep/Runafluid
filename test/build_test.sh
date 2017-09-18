97bafaf511fb3abcb437d8aa7735975e42691c4b
plasmatyi <aradimatyi@gmail.com>
2017-09-18 11:58:10 +0000

    [#1202] test/build_test.sh updated for IMAS

--- test/build_test.sh
+++ test/build_test.sh
@@ -3,9 +3,15 @@
 cd ..
 
 make dev
-
-fc2k test/fc2k/runafluid_neEdit.xml 
-fc2k test/fc2k/runafluid_teEdit.xml 
-fc2k test/fc2k/runafluid_qe_expdecay.xml
-#fc2k test/fc2k/runafluid_te_expdecay.xml
-#fc2k test/fc2k/runafluid_qeimpEdit.xml
+if ( ( $?ITM_ENVIRONMENT_LOADED) )  then
+    if ( $ITM_ENVIRONMENT_LOADED == yes)  then
+        echo "*** Compiler set to ITM ***"
+            fc2k test/fc2k/runafluid_neEdit.xml 
+            fc2k test/fc2k/runafluid_teEdit.xml 
+            fc2k test/fc2k/runafluid_qe_expdecay.xml
+    else
+        echo "*** Compiler set to IMAS ***"
+            fc2k test/fc2k_imas/runafluid_neEdit.xml 
+            fc2k test/fc2k_imas/runafluid_teEdit.xml 
+    endif
+endif
