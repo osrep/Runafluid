b7f96d202ecd78b1e932a0d9ea635d990126234e
olaszsoma <olaszsoma@gmail.com>
2019-04-02 12:02:35 +0200

    #1 test.sh message for missing GTEST variable was fixed

--- test.sh
+++ test.sh
@@ -6,16 +6,12 @@ if (  ( $?GTEST) ) then
     ./test.bin || exit
 
 else
-    echo "GTEST environmentally variable is not exist. \n"
-    echo "Set GTEST environmentally variable with the absolute path of googletest folder (add lines in .cshrc)"
-    echo "# google test init"
-    echo 'setenv GTEST $ITMWORK/gtest-1.8.0/googletest'
-    echo 'setenv LD_LIBRARY_PATH $LD_LIBRARY_PATH\:$GTEST\n'
+    echo "GTEST environmental variable does not exist. \n"
     echo "Please download the latest Google test from github\n https://github.com/google/googletest \n"
-    echo "Install the Google test"
-    echo "1. Go to googletest/make folder"
-    echo "2. run make command"
-    echo "3. Go to googletest folder"
-    echo '4. run cmake $GTEST command'
-    echo "5. run make command\n"
+    echo "Use the latest released version \n"
+    echo "Build the Google test"
+    echo "Set GTEST environmental variable with the absolute path of googletest folder (may add lines in automatic tcsh login script)"
+    echo "# google test init"
+    echo 'setenv GTEST /path/to/googletest'
+    echo 'setenv LD_LIBRARY_PATH $LD_LIBRARY_PATH\:$GTEST\n'
 endif
