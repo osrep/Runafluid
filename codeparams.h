e3a3e5b92cce790ad39a668f52a291b7195fcc72
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-06-17 13:44:27 +0200

    #17 runafluid module switches and runafluid_switch replaced to module_struct AND #18 runaway_rates temporary CPO removed

--- codeparams.h
+++ codeparams.h
@@ -3,9 +3,8 @@
 
 #include "control.h"
 
-int set_switch_from_codeparams(ItmNs::codeparam_t &codeparams);
-modules read_codeparams(ItmNs::codeparam_t &codeparams);
-modules simulate_codeparams(int runafluid_switch);
+module_struct read_codeparams(ItmNs::codeparam_t &codeparams);
+module_struct simulate_codeparams(int runafluid_switch);
 
 std::string stream_xml_string(std::string xml_string, std::string ref);
 std::string split_string(std::string s, std::string ref);
